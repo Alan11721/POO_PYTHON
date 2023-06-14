088343f3c2e877bbb4a4cd134f75f655526808b8
Alan11721 <111447249+Alan11721@users.noreply.github.com>
2023-06-14 16:25:03 -0600

    Add files via upload

--- cosas.py
+++ cosas.py
@@ -1,10 +1,110 @@
 class Alumno:
-    facultad = "fes aragon"
-    def __init__(self,nom, ed, carr):
-        #print("constructor")
-        #print(type(self))
+    facultad = "FES Aragón"
+
+    def __init__(self, nom, ed, carr):
         self.__nombre = nom
         self.__edad = ed
         self.__carrera = carr
+    def set_nombre(self,nom):
+        self.__nombre = nom
 
+    def get_nombre(self):
+        return self.__nombre
+
+    def set_edad(self,ed):
+        if ed >= 8 and ed < 120:
+            self.__edad = ed
+        else:
+            print("esa edad no es corecta")
+            self.__edad = ed
+    def get_edad(self):
+        return self.__edad
+
+    def set_carrera(self, carr):
+        self.__carrera = carr
+
+    def get_carrera(self):
+        return self.__carrera
+
+
+
+    def __str__(self):
+        cadena = "--------- \n nombre: " + self.__nombre
+        cadena= cadena + "\n edad:" + str(self. __edad)
+        cadena = cadena + "\n carrera:" + self.__carrera
+        cadena = cadena + "\n --------------"
+        return cadena
+
+    def estudiar(self,horas=1):
+        print(f"el alumno {self.__nombre} esta estudiando {horas}")
+
+
+class Perro:
+    reino = "Canino"
+
+    def __init__(self, raza, edad, estatura):
+        self.__raza = raza
+        self.__edad = edad
+        self.__estatura = estatura
+
+    #metodo de acceso get
+    @property
+    def raza(self):
+        return self.__raza
+
+    #metodo de acceso set
+    @raza.setter
+    def raza(self, la_raza):
+        self.__raza = la_raza
+
+    @property
+    def edad(self):
+        return self.__edad
+
+
+    @edad.setter
+    def edad(self, la_edad):
+        if la_edad > 0 and la_edad < 20:
+            self.__edad = la_edad
+        else:
+            print("esa edad no es valida")
+            self.__edad = 0
+    @property
+    def estatura(self):
+        return self.__estatura
+
+
+
+    @estatura.setter
+    def estatura(self, la_estatura):
+        if la_estatura > 0.1 and la_estatura < 1.1:
+            self.__estatura = la_estatura
+        else:
+            print("no way")
+            self.__estatura = 0.1
+    def __str__(self):
+        return f"""
+        __________________________
+        | RAZA : {self.__raza}          | 
+        | EDAD : {self.__edad}          | 
+        | ESTATURA : {self.__estatura}          | 
+        ----------------------------
+        """
+
+
+
+
+    @staticmethod
+    def es_cachorro(edad):
+        return edad < 3
+    @staticmethod
+    def dormir(veces = 5):
+        for n in range(veces):
+            print(f"dando vuelta{n + 1}")
+        print("Zzzzzz")
+
+    @classmethod
+    def perro_grande(cls, est):
+        if est > 0.79:
+            return cls ("",0, est)
 
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-import aritmetica as ari
+from cosas import Alumno, Perro
+
+
 def main():
-    resultado = ari.suma(3,4)
-    print(resultado)
-    print(ari.resta(b=5,a=3))
+    al1 = Alumno("jose", 19, "ICO")
+    print(vars(al1))
+    al1.__nombre = "diego"
+    print(vars(al1))
+    al1.set_nombre("maria")
+    print(vars(al1))
+    print("------To string ------")
+    print(al1)
+    al1.set_edad(999)
+    print(al1)
+    al1.estudiar(4)
+
+    print("------- PERRO --------")
+    perro1 = Perro("poddle", 2, 0.35)
+    perro1(vars(perro1))
+    perro1.raza = "de la calle"  # set en notacion pythonic
+    print(vars(perro1))
+    perro1.__raza = "otra"
+    print(vars(perro1))
+    perro1.edad = 12
+    perro1.estatura = 0.43
+    print(perro1)
+    cachorro = Perro.es_cachorro(perro1.edad)
+    print(cachorro)
+    Perro.dormir()
+    danes = Perro.perro_grande(0.8)
+    print(danes)
+
 
 main()
